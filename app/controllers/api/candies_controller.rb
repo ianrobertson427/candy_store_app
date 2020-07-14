6329ad912c225ad44516f77c94067897012dafec
“irobetson” <ianrobertson427@gmail.com>
2020-07-14 12:14:27 -0500

    Adding routes

--- app/controllers/api/candies_controller.rb
+++ app/controllers/api/candies_controller.rb
@@ -1,2 +1,36 @@
 class Api::CandiesController < ApplicationController
+  def index
+    @photos = Candy.all
+    render "index.json.jb"
+  end
+
+  def create
+    @candy = Candy.new(
+      name: params[:name],
+      price: params[:price],
+      brand: params[:brand],
+    )
+    @candy.save
+    render "show.json.jb"
+  end
+
+  def show
+    @candy = Candy.find_by(id: params[:id])
+    render "show.json.jb"
+  end
+
+  def update
+    @candy = Candy.find_by(id: params[:id])
+    @candy.name = params[:name] || @candy.name
+    @candy.width = params[:price] || @candy.price
+    @candy.brand = params[:brand] || @candy.brand
+    @candy.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    candy = Candy.find_by(id: params[:id])
+    candy.destroy
+    render json: { message: "Photo successfully destroyed." }
+  end
 end
